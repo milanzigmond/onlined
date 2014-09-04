fa4b823091217e5f3d3f5f5b11ff15374933b0ab
Milan Zigmond <milanzigmond@milanzigmond.com>
2014-09-04 17:53:56 +0200

    map almost working

--- onlined.js
+++ onlined.js
@@ -34,6 +34,7 @@ if (Meteor.isClient) {
     {name:'Yeti', css: 'yeti.css'},
   ]);
   Session.setDefault('alert', null);
+  Session.setDefault('autocomplete', null);
 
   if (Accounts._resetPasswordToken) {
     Session.set('resetPassword', Accounts._resetPasswordToken);
@@ -310,7 +311,61 @@ if (Meteor.isClient) {
       }
     });
 
+    var setupMap = function () {
+      var mapOptions = {
+        center: new google.maps.LatLng(-33.8688, 151.2195),
+        zoom: 13
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+      var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
+
+      // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+      });
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          console.log('place.geometry.location:'+place.geometry.location);
+          map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setIcon(/** @type {google.maps.Icon} */({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong>');
+        infowindow.open(map, marker);
+      });
+    };
+
     Template.create.rendered = function () {
+
+      setupMap();
+
       var slider = new Slider( $('div.sliderGallery ul'), $('#sliderGalleryNav'));
       slider.nav.find('button').on('click', function() {
         // console.log('clicked'+$(this).data('dir'));
